8254b9085f5a0fb46aa5b11e9941592d0ccd9611
Izyver <mr.izyver@gmail.com>
2019-05-05 13:24:03 +0300

    created permissions

---
+++ app/src/main/java/com/gatisnau/gati/GatiPermissions.java
@@ -0,0 +1,21 @@
+package com.gatisnau.gati;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class GatiPermissions {
+    public static boolean checkWritePermissions(Context context){
+        return ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void requestReadWrite(Activity activity, int requestCode){
+        ActivityCompat.requestPermissions(activity, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, requestCode);
+    }
+}
